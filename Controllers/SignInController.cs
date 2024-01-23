95babf4ab1075c8899daea68f5f89448db2f9eaf
salman1256 <salman1256@yahoo.com>
2024-01-23 10:49:48 +0530

    Secure MVC

---
+++ Controllers/SignInController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppSecure.Models;
+
+namespace WebAppSecure.Controllers
+{
+    public class SignInController : Controller
+    {
+        // GET: SignIn
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new SignIn());
+        }
+        [HttpPost]
+        public ActionResult Index(SignIn sign)
+        {
+            if (ModelState.IsValid)
+            {
+                if ((sign.UserName == "sam1256") && (sign.Password == "sam@1256@1256"))
+                {
+
+                    Session["UserId"] = Guid.NewGuid();
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Either User Name or Password Incorrect!!!");
+                    return View(sign);
+
+                }
+            }
+            else
+            { return View(sign); }
+
+        }
+    }
+}
+   
---
+++ Models/SignIn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppSecure.Models
+{
+    public class SignIn
+    {
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "User Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
